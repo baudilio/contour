b9083184fa73510cede850029fc7668bc20e8e3c
Baudilio Tejerina (@Vigo) <bltejerina@gmail.com>
2025-06-22 15:05:23 -0400

    Simplify IF statement blocks.

--- source/contour_lib.f90
+++ source/contour_lib.f90
@@ -47,136 +47,135 @@ subroutine conrec(d,ilb,iub,jlb,jub,x,y,nc,z)
         dmin = min(d(i,j),d(i,j+1),d(i+1,j),d(i+1,j+1))
         dmax = max(d(i,j),d(i,j+1),d(i+1,j),d(i+1,j+1))
 
-        BTA: if (dmax.ge.z(1) .and. dmin.le.z(nc)) then
+        ! BTA: if (dmax.ge.z(1) .and. dmin.le.z(nc)) then
+        if (dmax < z(1) .OR. dmin > z(nc)) CYCLE
 
-           CONTOUR: do k=1,nc
-              if (z(k).ge.dmin .and. z(k).le.dmax) then
-                 do m=4,0,-1
-                    if (m.gt.0) then
-                       h(m)=d(i+im(m),j+jm(m))-z(k)
-                       xh(m)=x(i+im(m))
-                       yh(m)=y(j+jm(m))
-                    else
-                       h(0)=0.25*(h(1)+h(2)+h(3)+h(4))
-                       xh(0)=0.5*(x(i)+x(i+1))
-                       yh(0)=0.5*(y(j)+y(j+1))
-                    endif
-                    if (h(m).gt.0.0) then
-                       sh(m)=+1
-                    else if (h(m).lt.0.0) then
-                       sh(m)=-1
-                    else
-                       sh(m)=0
-                    endif
-                 end do
-
-                 !
-                 !     Note: at this stage the relative heights of the corners and the
-                 !     centre are in the h array, and the corresponding coordinates are
-                 !     in the xh and yh arrays. The centre of the box is indexed by 0
-                 !     and the 4 corners by 1 to 4 as shown below.
-                 !     Each triangle is then indexed by the parameter m, and the 3
-                 !     vertices of each triangle are indexed by parameters m1,m2,and m3.
-                 !     It is assumed that the centre of the box is always vertex 2 though
-                 !     this isimportant only when all 3 vertices lie exactly on the same
-                 !     contour level, in which case only the side of the box is drawn.
-                 !
-                 !
-                 !           vertex 4 +-------------------+ vertex 3
-                 !                    | \               / |
-                 !                    |   \    m-3    /   |
-                 !                    |     \       /     |
-                 !                    |       \   /       |
-                 !                    |  m=2    X   m=2   |       the centre is vertex 0
-                 !                    |       /   \       |
-                 !                    |     /       \     |
-                 !                    |   /    m=1    \   |
-                 !                    | /               \ |
-                 !           vertex 1 +-------------------+ vertex 2
-                 !
-                 !
-                 !
-                 !                    Scan each triangle in the box
-                 !
-                 DO60:  do m=1,4
-                    m1=m
-                    m2=0
-                    m3=1
-                    if (m .ne. 4) m3 = m + 1
-
-                    case = castab(sh(m1),sh(m2),sh(m3))
-
-                    select case (case)
-
-                    case(0)
-                       cycle DO60
-
-                    case (1) ! Case 1 - Line between vertices 1 and 2
-                       x1=xh(m1)
-                       y1=yh(m1)
-                       x2=xh(m2)
-                       y2=yh(m2)
-
-                    case (2) ! Case 2 - Line between vertices 2 and 3
-                       x1=xh(m2)
-                       y1=yh(m2)
-                       x2=xh(m3)
-                       y2=yh(m3)
-
-                    case (3) ! Case 3 - Line between vertices 3 and 1
-                       x1=xh(m3)
-                       y1=yh(m3)
-                       x2=xh(m1)
-                       y2=yh(m1)
-
-                    case (4) ! Case 4 - Line between vertex 1 and side 2-3
-                       x1=xh(m1)
-                       y1=yh(m1)
-                       x2=xsect(m2,m3)
-                       y2=ysect(m2,m3)
-
-                    case (5) ! Case 5 - Line between vertex 2 and side 3-1
-                       x1=xh(m2)
-                       y1=yh(m2)
-                       x2=xsect(m3,m1)
-                       y2=ysect(m3,m1)
-
-                    case (6) ! Case 6 - Line between vertex 3 and side 1-2
-                       x1=xh(m3)
-                       y1=yh(m3)
-                       x2=xsect(m1,m2)
-                       y2=ysect(m1,m2)
-
-                    case (7) ! Case 7 - Line between sides 1-2 and 2-3
-                       x1=xsect(m1,m2)
-                       y1=ysect(m1,m2)
-                       x2=xsect(m2,m3)
-                       y2=ysect(m2,m3)
-
-                    case (8) ! Case 8 - Line between sides 2-3 and 3-1
-                       x1=xsect(m2,m3)
-                       y1=ysect(m2,m3)
-                       x2=xsect(m3,m1)
-                       y2=ysect(m3,m1)
-
-                    case (9) ! Case 9 - Line between sides 3-1 and 1-2
-                       x1=xsect(m3,m1)
-                       y1=ysect(m3,m1)
-                       x2=xsect(m1,m2)
-                       y2=ysect(m1,m2)
-
-                    case default
-                       print *, " Something went wrong - IMPOSIBLE CASE: ", case
-                       call exit(1)
-
-                    end select
-
-                    call vecout(x1,y1,x2,y2,z(k))
-
-                 end do DO60  ! 60 continue
+        CONTOUR: do k=1,nc
+           if (z(k) < dmin .or. z(k) > dmax) cycle CONTOUR
+           do m=4,0,-1
+              if (m.gt.0) then
+                 h(m)=d(i+im(m),j+jm(m))-z(k)
+                 xh(m)=x(i+im(m))
+                 yh(m)=y(j+jm(m))
+              else
+                 h(0)=0.25*(h(1)+h(2)+h(3)+h(4))
+                 xh(0)=0.5*(x(i)+x(i+1))
+                 yh(0)=0.5*(y(j)+y(j+1))
               endif
-           end do CONTOUR
-        endif BTA
+              if (h(m).gt.0.0) then
+                 sh(m)=+1
+              else if (h(m).lt.0.0) then
+                 sh(m)=-1
+              else
+                 sh(m)=0
+              endif
+           end do
+
+           !
+           !     Note: at this stage the relative heights of the corners and the
+           !     centre are in the h array, and the corresponding coordinates are
+           !     in the xh and yh arrays. The centre of the box is indexed by 0
+           !     and the 4 corners by 1 to 4 as shown below.
+           !     Each triangle is then indexed by the parameter m, and the 3
+           !     vertices of each triangle are indexed by parameters m1,m2,and m3.
+           !     It is assumed that the centre of the box is always vertex 2 though
+           !     this isimportant only when all 3 vertices lie exactly on the same
+           !     contour level, in which case only the side of the box is drawn.
+           !
+           !
+           !           vertex 4 +-------------------+ vertex 3
+           !                    | \               / |
+           !                    |   \    m-3    /   |
+           !                    |     \       /     |
+           !                    |       \   /       |
+           !                    |  m=2    X   m=2   |       the centre is vertex 0
+           !                    |       /   \       |
+           !                    |     /       \     |
+           !                    |   /    m=1    \   |
+           !                    | /               \ |
+           !           vertex 1 +-------------------+ vertex 2
+           !
+           !
+           !
+           !                    Scan each triangle in the box
+           !
+           DO60:  do m=1,4
+              m1=m
+              m2=0
+              m3=1
+              if (m .ne. 4) m3 = m + 1
+
+              case = castab(sh(m1),sh(m2),sh(m3))
+
+              select case (case)
+
+              case(0)
+                 cycle DO60
+
+              case (1) ! Case 1 - Line between vertices 1 and 2
+                 x1=xh(m1)
+                 y1=yh(m1)
+                 x2=xh(m2)
+                 y2=yh(m2)
+
+              case (2) ! Case 2 - Line between vertices 2 and 3
+                 x1=xh(m2)
+                 y1=yh(m2)
+                 x2=xh(m3)
+                 y2=yh(m3)
+
+              case (3) ! Case 3 - Line between vertices 3 and 1
+                 x1=xh(m3)
+                 y1=yh(m3)
+                 x2=xh(m1)
+                 y2=yh(m1)
+
+              case (4) ! Case 4 - Line between vertex 1 and side 2-3
+                 x1=xh(m1)
+                 y1=yh(m1)
+                 x2=xsect(m2,m3)
+                 y2=ysect(m2,m3)
+
+              case (5) ! Case 5 - Line between vertex 2 and side 3-1
+                 x1=xh(m2)
+                 y1=yh(m2)
+                 x2=xsect(m3,m1)
+                 y2=ysect(m3,m1)
+
+              case (6) ! Case 6 - Line between vertex 3 and side 1-2
+                 x1=xh(m3)
+                 y1=yh(m3)
+                 x2=xsect(m1,m2)
+                 y2=ysect(m1,m2)
+
+              case (7) ! Case 7 - Line between sides 1-2 and 2-3
+                 x1=xsect(m1,m2)
+                 y1=ysect(m1,m2)
+                 x2=xsect(m2,m3)
+                 y2=ysect(m2,m3)
+
+              case (8) ! Case 8 - Line between sides 2-3 and 3-1
+                 x1=xsect(m2,m3)
+                 y1=ysect(m2,m3)
+                 x2=xsect(m3,m1)
+                 y2=ysect(m3,m1)
+
+              case (9) ! Case 9 - Line between sides 3-1 and 1-2
+                 x1=xsect(m3,m1)
+                 y1=ysect(m3,m1)
+                 x2=xsect(m1,m2)
+                 y2=ysect(m1,m2)
+
+              case default
+                 print *, " Something went wrong - IMPOSIBLE CASE: ", case
+                 call exit(1)
+
+              end select
+
+              call vecout(x1,y1,x2,y2,z(k))
+
+           end do DO60  ! 60 continue
+        end do CONTOUR
      end do RX ! 90  continue
   end do RY ! 100 continue
 
